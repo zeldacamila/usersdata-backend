34d1baef88335394048ffe4e3facc891a4357fc8
Maria <zeldacamila0913@gmail.com>
2023-04-02 12:30:44 -0500

    first line of file server.js commented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const { connect } = require('./db');
 const adminRoute = require('./api/admin/admin.route')
 
 const app = express()
 const port = process.env.PORT
 connect();
+
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}))
 
 app.use(express.json())
 
