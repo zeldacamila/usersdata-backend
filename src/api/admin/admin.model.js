9caf0cdfa2a75dcfb5f11f4e91704dfebef5f6b9
Maria <zeldacamila0913@gmail.com>
2023-04-11 11:13:48 -0500

    admin model updated

--- src/api/admin/admin.model.js
+++ src/api/admin/admin.model.js
@@ -3,7 +3,8 @@ const { Schema, model } = require("mongoose")
 const adminSchema = new Schema({
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
